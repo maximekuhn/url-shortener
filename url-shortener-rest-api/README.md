84ec05a2e44d5830d0c9505d3dd78593cb0fe19c
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2023-09-16 12:50:14 +0200

    Set up axum

---
+++ url-shortener-rest-api/README.md
@@ -0,0 +1,4 @@
+# url-shortener-rest-api
+
+This (binary) crate contains the (executable) code to deploy a REST API capable of shortening
+and resolving URLs.
--- url-shortener-rest-api/src/main.rs
+++ url-shortener-rest-api/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Json, Router, Server};
+use axum::body::Body;
+use axum::http::Request;
+use axum::routing::get;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct URLRequest {
+    url: String,
+}
+
+async fn index(Json(payload): Json<URLRequest>) {
+    println!("ENDPOINT: /");
+    println!("body: {:?}", payload.url);
+}
+
+#[tokio::main]
+async fn main() {
+    let app =
+        Router::new()
+            .route("/", get(index));
+
+    Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
